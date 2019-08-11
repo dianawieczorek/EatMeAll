3a1eb83c5a6d4a090792be1ced83cf6e84f9770d
mentordjang <dianaktowica@wp.pl>
2019-08-11 08:40:53 +0200

    delete practise code

--- src/Redux/bundle.ts
+++ src/Redux/bundle.ts
@@ -1,6 +1,6 @@
 import {combineReducers} from "redux";
-import {listReducer} from "./reducers";
+import {weekScheduleReducer} from "./reducers";
 
 export const rootReducer = combineReducers({
-    listReducer
+    weekScheduleReducer
 });
--- src/Redux/reducers.ts
+++ src/Redux/reducers.ts
@@ -1,7 +1,6 @@
 import {ADD_ITEM, Types} from "./actionTypes";
 import {Reducer} from "redux";
-import produce from 'immer';
-
+import {produce} from "immer"
 
 // interface InitialState {
 //     itemList: string[]
@@ -10,33 +9,31 @@ import produce from 'immer';
 // const INITIAL_STATE: InitialState = {
 //     itemList: ["test1-redux", "test2-redux"]
 // };
-
-
+//
+//
 // export const listReducer: Reducer<InitialState, Types> = (state: InitialState = INITIAL_STATE, action: Types) => {
 //     switch (action.type) {
 //         case ADD_ITEM: {
 //             return produce(state, draftState => {
 //                 draftState.itemList.push(action.newItem)
-//             }
-//             }
+//             })
 //         }
 //         default:
 //             return state
-//     };
-// };
+//     }
+// }
+
 interface weekInitialState {
 }
 
-const WEEK_INITIAL_STATE: weekInitialState = {
-};
+const WEEK_INITIAL_STATE: weekInitialState = {};
 
 export const weekScheduleReducer: Reducer<weekInitialState, Types> = (state: weekInitialState = WEEK_INITIAL_STATE, action: Types) => {
     switch (action.type) {
         case ADD_ITEM: {
-            return {
-
-            }
+            return {}
         }
         default:
             return state
-};
+    }
+}
