4e6cf414cf37a373aabdeb5b5497f066537d47ae
mentordjang <dianaktowica@wp.pl>
2019-08-13 10:09:22 +0200

    generate default name

--- src/Components/WeekSchedule/TableContent/SingleMealShortInfo/SingleMealShortInfo.tsx
+++ src/Components/WeekSchedule/TableContent/SingleMealShortInfo/SingleMealShortInfo.tsx
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 import {AppStore} from "../../../../Redux/store";
 import {Dispatch} from "redux";
 import styles from "./SingleMealShortInfo.module.css"
+import {element} from "prop-types";
+import ReactDOM from 'react-dom';
 
 
 interface OwnProps {
@@ -11,33 +13,37 @@ interface OwnProps {
 type Props = OwnProps & ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
 class SingleMealShortInfo extends PureComponent<Props> {
+    state = {
+        mealName: ''
+    };
+
     render() {
         this.dupa()
         return (
+
             <div className={styles.MealInfo}>
                 <div className={styles.TypeOfMeal}>śniadanko</div>
-                <div>tatusiowy twarożek</div>
+                <div>{this.state.mealName}</div>
             </div>
         );
     }
 
     private dupa = () => {
-        // let WeekMeal: any = this.props.currentWeekSchedule
-        // if (WeekMeal !== undefined) {
-        //     return (
-        //         console.log(WeekMeal[0]["meals"][0]["title"]))
-        // }
+        let MealIfo: any = this.props.MealInfo;
+        if (MealIfo !== undefined) {
+            return (
+                this.state.mealName = MealIfo.title
+            )
+        }
     }
 }
 
 
 const mapStateToProps = (state: AppStore) => {
 
-    if(state.weekScheduleReducer.currentWeekSchedule!==undefined){
-        console.log(state.weekScheduleReducer.currentWeekSchedule)
-        console.log(state.weekScheduleReducer.currentWeekSchedule[0].meals[0])
+    if (state.weekScheduleReducer.currentWeekSchedule !== undefined) {
         return {
-            x: state.weekScheduleReducer.currentWeekSchedule
+            MealInfo: state.weekScheduleReducer.currentWeekSchedule[0].meals[0]
         }
 
     }
