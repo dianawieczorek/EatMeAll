849fa615a7a0d03522efeef020f43823a77b0b87
mentordjang <dianaktowica@wp.pl>
2019-08-14 10:33:31 +0200

    added buttons to meals

--- src/Components/WeekSchedule/TableContent/SingleMealShortInfo/SingleMealShortInfo.tsx
+++ src/Components/WeekSchedule/TableContent/SingleMealShortInfo/SingleMealShortInfo.tsx
@@ -5,10 +5,12 @@ import {Dispatch} from "redux";
 import styles from "./SingleMealShortInfo.module.css"
 import {element} from "prop-types";
 import ReactDOM from 'react-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBook, faCalendarTimes, faCopy, faListUl, faPaste, faRetweet} from "@fortawesome/free-solid-svg-icons";
 
 
 interface OwnProps {
-    mealNumber:number
+    mealNumber: number
 }
 
 type Props = OwnProps & ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
@@ -17,38 +19,60 @@ class SingleMealShortInfo extends PureComponent<Props> {
     render() {
         if (this.props.MealInfo !== undefined) {
             return (
-                <div className={styles.MealInfo}>
-                    <div className={styles.TypeOfMeal}>{this.typeOfMeal()}</div>
-                    <div>{this.props.MealInfo.title}</div>
+                <div className={styles.ChangeDiv}>
+                    <div className={styles.MealInfo}>
+                        <div className={styles.TypeOfMeal}>{this.typeOfMeal()}</div>
+                        <div className={styles.MealName}>{this.props.MealInfo.title}</div>
+                        <div className={styles.HoverButtons}>
+                            <button className={styles.Button} /*onClick={this.showDetails}*/>
+                                <FontAwesomeIcon className={styles.Icon} icon={faBook} title="przeczytaj przepis"/>
+                            </button>
+                            <button className={styles.Button} /*onClick={this.randomizeMeal}*/>
+                                <FontAwesomeIcon className={styles.Icon} icon={faRetweet} title="wylosuj inną potrawę"/>
+                            </button>
+                            <button className={styles.Button} /*onClick={this.changeMealFromList}*/>
+                                <FontAwesomeIcon className={styles.Icon} icon={faListUl} title="wybierz inną potrawę z listy"/>
+                            </button>
+                            <button className={styles.Button} /*onClick={this.deleteMealFromList}*/>
+                                <FontAwesomeIcon className={styles.Icon} icon={faCalendarTimes} title="wyczyść/jem na mieście"/>
+                            </button>
+                            <button className={styles.Button} /*onClick={this.copyActionHandler}*/>
+                                <FontAwesomeIcon className={styles.Icon} icon={faCopy} title="kopiuj"/>
+                            </button>
+                            <button className={styles.Button} /*onClick={this.pasteActionHandler}*/>
+                                <FontAwesomeIcon className={styles.Icon} icon={faPaste} title="wklej"/>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             );
         } else {
             return (
                 <div className={styles.MealInfo}>
-                    <div className={styles.TypeOfMeal}> </div>
-                    <div> </div>
+                    <div className={styles.TypeOfMeal}></div>
+                    <div></div>
                 </div>
             )
         }
     }
 
     private typeOfMeal = () => {
-        if (this.props.MealInfo.mealTime=="BREAKFAST"){
+        if (this.props.MealInfo.mealTime == "BREAKFAST") {
             return "śniadanko"
         } else if (this.props.MealInfo.mealTime == "DINNER") {
             return "obiad"
         } else if (this.props.MealInfo.mealTime == "LUNCH") {
             return "drugie śniadanie"
-        }else if (this.props.MealInfo.mealTime == "SNACK") {
+        } else if (this.props.MealInfo.mealTime == "SNACK") {
             return "podwieczorek"
-        }else if (this.props.MealInfo.mealTime == "SUPPER") {
+        } else if (this.props.MealInfo.mealTime == "SUPPER") {
             return "kolacja"
         }
     }
 }
 
 
-const mapStateToProps = (state: AppStore, ownProps:any) => {
+const mapStateToProps = (state: AppStore, ownProps: any) => {
     if (state.weekScheduleReducer.currentWeekSchedule !== undefined) {
         return {
             MealInfo: state.weekScheduleReducer.currentWeekSchedule[ownProps.dayNumber].meals[ownProps.mealNumber]
