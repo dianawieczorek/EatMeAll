8b8307ff1ca0efaef146e7e2695c6f44822d16bf
mentordjang <dianaktowica@wp.pl>
2019-08-19 06:03:08 +0200

    created MealRecipe view with static data

--- src/Components/MealRecipe/MealRecipe.tsx
+++ src/Components/MealRecipe/MealRecipe.tsx
@@ -1,16 +1,40 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React, {PureComponent} from 'react';
+import {connect} from 'react-redux';
+import styles from './MealRecipe.module.css'
 
-interface OwnProps {}
+interface OwnProps {
+}
 
 type Props = OwnProps & ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
 class MealRecipe extends PureComponent<Props> {
-  render() {
-    return (
-      <div>Przepis</div>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <h2>Nazwa Przepisu</h2>
+                <div className={styles.ProductAndInfo}>
+                    <div className={styles.BasicInfo}>
+                        <p>rodzaj dania:</p>
+                        <p>czas przygotowania:</p>
+                        <p>ilość kcal:</p>
+                        <p>autor przepisu:</p>
+                    </div>
+                    <div className={styles.Products}>
+                        <h4 className={styles.MealRecipe}>Składniki</h4>
+                        <ul>
+                            <li>składnik</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.Preparation}>
+                    <h4>Sposób wykonania</h4>
+                    <ol>
+                        <li>zrób to i to</li>
+                    </ol>
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 
 const mapStateToProps = () => {
