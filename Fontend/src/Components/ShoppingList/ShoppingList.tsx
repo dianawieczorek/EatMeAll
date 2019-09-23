8193c7882c8ac1c2b026c41ace910251c846bb84
mentordjang <dianaktowica@wp.pl>
2019-09-23 13:56:08 +0200

    checkbox template for shopping list created

--- Fontend/src/Components/ShoppingList/ShoppingList.tsx
+++ Fontend/src/Components/ShoppingList/ShoppingList.tsx
@@ -1,8 +1,8 @@
 import React, {PureComponent} from 'react';
 import {connect} from 'react-redux';
-import styles from "./ShoppingList.module.css" ;
 import classes from "../WeekSchedule/TableHeader/TableHeader.module.css";
 import style from '..//WeekSchedule/WeekSchedule.module.css'
+import styles from "./ShoppingList.module.css"
 import {AppStore} from "../../Redux/store";
 import Button from "../UI/Button/Button"
 import CategoryListOfProduct from "./CategoryListOfProducts/CategoryListOfProduct";
@@ -23,10 +23,34 @@ class ShoppingList extends PureComponent<Props> {
     }
 
     render() {
+        let days = ["Pon", "Wt", "Śr", "Czw", "Pt", "Sob", "Nd"]
+
         return (
             <div className={style.PageSettings}>
                 <div className={classes.TableHeader}>
                     <div className={classes.Label}>Lista Zakupów</div>
+                    <div className={styles.Checkbox}>
+                        {this.props.memberList.map((userName: string) =>
+                            <React.Fragment>
+                                <div className={styles.MemberList}>
+                                    <p>wygeneruj listę zakupów dla:</p>
+                                    <div>
+                                        <input value={userName} type="checkbox"/>
+                                        <label>{userName}</label>
+                                        <div>
+                                            {days.map((day: string) =>
+                                                <div className={styles.DayList}>
+                                                    <input value={day} type="checkbox"/>
+                                                    <label>{day}</label>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </React.Fragment>
+                        )}
+                    </div>
                     <div className={classes.Buttons}>
                         <Button onClick={this.shoppingList}>
                             Wygeneruj listę zakupów
@@ -50,25 +74,27 @@ class ShoppingList extends PureComponent<Props> {
     }
 
     private shoppingList = () => {
-        // let mealIds = [this.props.Meal.filter((dayOfWeekPlan,i) => x.contains(i)).map(dayOfWeekPlan => dayOfWeekPlan["meals"].map(meal => meal.idMeal))];
-        // let mealIds=[this.props.Meal.map((dayOfWeekPlan: any) => dayOfWeekPlan["meals"].map((meal: any) => meal.idMeal))][0];
-        let mealIds=[this.props.Meal.map((dayOfWeekPlan: any) => dayOfWeekPlan["meals"].map((meal: any) => meal.idMeal))][0];
-        // let arrayOfMealIds= mealIds[0].concat(mealIds[1]).concat(mealIds[2]).concat(mealIds[3]).concat(mealIds[4]).concat(mealIds[5]).concat(mealIds[6]);
-        let arrayOfMealIds= [...mealIds[0],...mealIds[1],...mealIds[2],...mealIds[3],...mealIds[4],...mealIds[5],...mealIds[6]];
-        fetch("http://217.182.78.23:100/app/shoppingList/order/id/" + arrayOfMealIds)
-            .then((response) => response.json())
-            .then((json: GroupproductsDto) => {
-                    this.props.setProductList(json)
-                }
-            );
+        // let mealIds = [this.props.Meal.filter((dayOfWeekPlan,i) => x.contains(i)).map(dayOfWeekPlan => dayOfWeekPlan.weekSchedule["meals"].map((meal => meal.idMeal))];
+        let mealIds = [this.props.Meal.map((member: any) => member.weekSchedule.map((dayOfWeekPlan: any) => dayOfWeekPlan["meals"].map((meal: any) => meal.idMeal)))];
+        console.log(mealIds)
+        // let selectedMember = mealIds[n]
+        // let arrayOfMealIds= [...mealIds[0],...mealIds[1],...mealIds[2],...mealIds[3],...mealIds[4],...mealIds[5],...mealIds[6]];
+        // console.log(arrayOfMealIds)
+        // fetch("http://217.182.78.23:100/app/shoppingList/order/id/" + arrayOfMealIds)
+        //     .then((response) => response.json())
+        //     .then((json: GroupproductsDto) => {
+        //             this.props.setProductList(json)
+        //         }
+        //     );
     }
 }
 
-const mapStateToProps = (state: AppStore) => {
-    if (state.weekScheduleReducer.members !== undefined) {
+const mapStateToProps = (store: AppStore) => {
+    if (store.weekScheduleReducer.members !== undefined) {
         return {
-            Meal: state.weekScheduleReducer.members[0].weekSchedule,
-            ProductList: state.productListReducer.categoryListOfProduct
+            Meal: store.weekScheduleReducer.members,
+            memberList: store.weekScheduleReducer.members.map(member => member.name),
+            ProductList: store.productListReducer.categoryListOfProduct
         }
     }
 };
