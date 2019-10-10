025e4f04b8f0ab56f246c0b90e5a7abc9f9eb84c
software-wizard <przemek@software-wizard.pl>
2019-10-10 06:42:58 +0200

    merge backend and docker

---
+++ backend/project/db-connector/src/main/java/pl/wizard/software/diet/products/ProductType.java
@@ -0,0 +1,52 @@
+//package pl.wizard.software.diet.products;
+//
+//import lombok.Data;
+//import pl.wizard.software.AbstractBaseEntity;
+//
+//import javax.persistence.*;
+//import java.util.Set;
+//
+//@Entity
+//@Data
+//public class ProductType extends AbstractBaseEntity {
+//
+//    @OneToMany(mappedBy = "productType")
+//    private Set<ProductEntity> products;
+////    private String productType;
+//    @Enumerated(EnumType.ORDINAL)
+//    private ProductTypeEnum productType;
+//
+//    public enum ProductTypeEnum {
+//        CEREALS("zboża"), DAIRY("przetwory mleczne"), HAMS("wędliny"), RAW_MEAT("mięso"), POULTRY("drób"), EGGS("jajka"), FATS("tłuszcze"), MUSHROMS("grzyby"), NUTS("orzechy"),
+//        FRUITS("owoce"), SPICES("przyprawy"), FISH("ryby"), VEGETABLES("warzywa"), ALCOHOLS("alkohole"), DRINKS("napoje"), SWEETS("słodycze"), OTHERS("inne");
+//
+//        private String type;
+//
+//        ProductTypeEnum(String aName) {
+//            this.type = aName;
+//        }
+//
+//        public String getType() {
+//            return type;
+//        }
+//
+//        public static ProductType.ProductTypeEnum parse(String aName) {
+//            ProductType.ProductTypeEnum type = null; // Default
+//            for (ProductType.ProductTypeEnum item : ProductType.ProductTypeEnum.values()) {
+//                if (item.getType().equals(aName)) {
+//                    type = item;
+//                    break;
+//                }
+//            }
+//            return type;
+//        }
+//    }
+//
+//    public ProductTypeEnum getProductType() {
+//        return productType;
+//    }
+//
+//    public void setProductType(String aProductType) {
+//        productType = ProductType.ProductTypeEnum.parse(aProductType);
+//    }
+//}
---
+++ backend/project/rest.base/src/main/java/pl/wizard/software/diet/ProductController.java
@@ -0,0 +1,9 @@
+package pl.wizard.software.diet;
+
+//@Controller
+//public class ProductController {
+//    @GetMapping("/")
+//    public String list(){
+//        return "products";
+//    }
+//}
