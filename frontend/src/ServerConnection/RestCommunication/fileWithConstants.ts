dd3665259f76b86439acadaa84fc72d50dd1aece
Diana Wieczorek <wieczorek.diana@outlook.com>
2019-11-13 19:32:56 +0100

    changed url to get products from database instead of a file

--- frontend/src/ServerConnection/RestCommunication/fileWithConstants.ts
+++ frontend/src/ServerConnection/RestCommunication/fileWithConstants.ts
@@ -2,7 +2,7 @@ export const BASIC_URL = 'http://localhost:8080/api/v1/';
 export const SHOPPING_LIST_URL = BASIC_URL + 'shoppingList/order/id/';
 export const SHOW_DETAIL_URL = BASIC_URL + 'meal/';
 export const RANDOM_MEAL_URL = BASIC_URL+ 'meal/random?count=1&mealTime=';
-export const ALL_PRODUCTS_INFORMATION = 'jsonMocks/allProducts.json';
+export const ALL_PRODUCTS_INFORMATION = BASIC_URL+'products';
 export const SINGLE_PRODUCT_INFORMATION = 'jsonMocks/singleProductData.json';
 export const SCHEDULE = BASIC_URL +'schedule';
 
