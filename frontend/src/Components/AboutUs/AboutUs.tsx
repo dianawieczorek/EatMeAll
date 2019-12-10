74b8d623cda7b49bb78e33ad8480c4a2bfa50118
Diana Wieczorek <wieczorek.diana@outlook.com>
2019-12-10 08:09:21 +0100

    add section about us

---
+++ frontend/src/Components/AboutUs/AboutUs.tsx
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react';
+
+class AboutUs extends PureComponent {
+    render() {
+        return (
+            <React.Fragment>
+                <div>Diana</div>
+                <div>Przemek</div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default AboutUs;
