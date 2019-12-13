85b5734c5fbecde7469285a7ca8ced123dc389a1
Diana Wieczorek <wieczorek.diana@outlook.com>
2019-12-13 06:38:36 +0100

    added linkedin profile

--- frontend/src/Components/AboutUs/AboutUs.tsx
+++ frontend/src/Components/AboutUs/AboutUs.tsx
@@ -1,11 +1,12 @@
 import React, {PureComponent} from 'react';
+import styles from "./AboutUs.module.css";
 
 class AboutUs extends PureComponent {
     render() {
         return (
             <React.Fragment>
-                <div>Diana</div>
-                <div>Przemek</div>
+                <div className={styles.PersonInfo}>Frontend - Diana <a href="https://www.linkedin.com/in/dianawieczorek/" target="_blank">linkedin profile</a></div>
+                <div className={styles.PersonInfo}>Backend - Przemek <a href="https://www.linkedin.com/in/przemys%C5%82aw-wieczorek-79815699//" target="_blank">linkedin profile</a></div>
             </React.Fragment>
         )
     }
