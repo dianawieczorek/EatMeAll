d10ed729d9146058d4cdd972bac397444af96faf
software-wizard <przemek@software-wizard.pl>
2019-12-22 20:18:14 +0100

    change urls

--- frontend/src/ServerConnection/RestCommunication/fileWithConstants.ts
+++ frontend/src/ServerConnection/RestCommunication/fileWithConstants.ts
@@ -3,7 +3,6 @@ export const SHOPPING_LIST_URL = BASIC_URL + 'shoppingList/';
 export const SHOW_DETAIL_URL = BASIC_URL + 'meal/';
 export const RANDOM_MEAL_URL = BASIC_URL+ 'meal/random?count=1&mealTime=';
 export const ALL_PRODUCTS_INFORMATION = BASIC_URL+'products';
-export const SINGLE_PRODUCT_INFORMATION = 'jsonMocks/singleProductData.json';
 export const SCHEDULE = BASIC_URL +'schedule';
-
-
+export const GET_PRODUCT_BY_ID_URL = BASIC_URL + 'products/';
+export const POST_MEAL_TO_SERVER_URL = BASIC_URL + 'meal/';
